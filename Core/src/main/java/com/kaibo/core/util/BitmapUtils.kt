daa83094ea0820b02b03587e8f787f43847591a4
王开波 <568966289@qq.com>
2018-10-15 23:31:31 +0800

    实现播放界面背景模糊已经启动退出动画

--- Core/src/main/java/com/kaibo/core/util/BitmapUtils.kt
+++ Core/src/main/java/com/kaibo/core/util/BitmapUtils.kt
@@ -1,8 +1,20 @@
 package com.kaibo.core.util
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.renderscript.Allocation
+import android.renderscript.Element
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicBlur
 import android.util.Base64
+import android.util.Log
+import androidx.annotation.FloatRange
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 /**
  * @author kaibo
@@ -19,4 +31,119 @@ fun Bitmap.toBase64(): String {
         // 转换为字符串
         return Base64.encodeToString(it.toByteArray(), Base64.DEFAULT)
     }
+}
+
+private const val TAG = "BitmapUtilKt"
+
+/**
+ * 将 Bitmap 保存到指定的文件
+ * @param file 保存到指定的文件
+ */
+fun Bitmap.saveToFile(file: File) {
+    if (file.exists()) {
+        file.delete()
+    }
+    val out = FileOutputStream(file)
+    this.compress(Bitmap.CompressFormat.PNG, 90, out)
+    out.flush()
+    out.close()
+}
+
+/**
+ * 从磁盘上获取Bitmap
+ *
+ * @param path
+ * @param name
+ * @return
+ */
+fun File.toBitmap(): Bitmap = BitmapFactory.decodeStream(FileInputStream(this))
+
+/**
+ * 自定义模糊背景
+ * 耗时操作    请在子线程执行
+ *
+ * @param scale  缩放大小 (>1.0), 例如若想将输入图片放大2倍然后再模糊(提高模糊效率), 则传入2.
+ * @param radius       缩放半径 (0.0 , 25.0]
+ * @param context
+ * @param mInputBitmap
+ * @param mTargetView
+ * @return
+ */
+fun Bitmap.blur(context: Context,
+                @FloatRange(from = 1.0, fromInclusive = false) scale: Float = 10.0f,
+                @FloatRange(from = 0.0, to = 25.0, fromInclusive = false) radius: Float = 25f): Bitmap {
+    if (scale < 1F) {
+        throw IllegalArgumentException("Value must be > 0.0 (was $scale)")
+    }
+
+    if (radius <= 0.0f || radius > 25.0f) {
+        throw IllegalArgumentException("Value must be > 0.0 and ≤ 25.0 (was $radius)")
+    }
+
+    val startTime = System.currentTimeMillis()
+    //首先将图片缩小一下  1.提高模糊效率   2.防止内存溢出
+    val outputBitmap = this.resizeByScale(1.0F / scale)
+    val rs = RenderScript.create(context)
+    val input = Allocation.createFromBitmap(rs, outputBitmap)
+    val output = Allocation.createTyped(rs, input.type)
+
+    //开始模糊操作
+    ScriptIntrinsicBlur
+            .create(rs, Element.U8_4(rs))
+            .run {
+                setRadius(radius)
+                setInput(input)
+                forEach(output)
+            }
+
+    output.copyTo(outputBitmap)
+    rs.destroy()
+    Log.d(TAG, "模糊一共耗时:${(System.currentTimeMillis() - startTime)}ms")
+    return outputBitmap
+}
+
+/**
+ * 按比例缩放bitmap
+ *
+ * @param bitmap
+ * @param scale  scale>1是放大   scale<1  是缩小
+ * @return
+ */
+fun Bitmap.resizeByScale(scale: Float): Bitmap {
+    if (scale <= 0) {
+        throw IllegalArgumentException("scale must be > 0")
+    }
+
+    val width = this.width
+    val height = this.height
+
+    val newWidth = width * scale
+    val newHeight = height * scale
+
+    val scaleWidth = newWidth / width
+    val scaleHeight = newHeight / height
+
+    val matrix = Matrix()
+    matrix.postScale(scaleWidth, scaleHeight)
+    return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
+}
+
+/**
+ * 指定尺寸缩放bitmap
+ *
+ * @param bitmap
+ * @param w
+ * @param h
+ * @return
+ */
+fun Bitmap.resizeBySize(w: Int, h: Int): Bitmap {
+    val width = this.width
+    val height = this.height
+
+    val scaleWidth = w.toFloat() / width
+    val scaleHeight = h.toFloat() / height
+
+    val matrix = Matrix()
+    matrix.postScale(scaleWidth, scaleHeight)
+    return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
 }
--- app/src/main/java/com/kaibo/music/activity/PlayActivity.kt
+++ app/src/main/java/com/kaibo/music/activity/PlayActivity.kt
@@ -1,13 +1,23 @@
 package com.kaibo.music.activity
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
 import com.jakewharton.rxbinding2.view.clicks
 import com.kaibo.core.activity.BaseActivity
+import com.kaibo.core.util.blur
 import com.kaibo.core.util.statusBarHeight
+import com.kaibo.core.util.toMainThread
 import com.kaibo.music.R
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_play.*
 import kotlinx.android.synthetic.main.include_play_top_layout.*
+import org.jetbrains.anko.backgroundDrawable
 
 /**
  * @author kaibo
@@ -19,17 +29,91 @@ import kotlinx.android.synthetic.main.include_play_top_layout.*
 
 class PlayActivity : BaseActivity() {
 
+    private val topLayoutIn: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.top_layout_in).apply {
+            fillAfter = true
+        }
+    }
+
+    private val topLayoutOut: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.top_layout_out).apply {
+            fillAfter = true
+        }
+    }
+
+    private val bottomLayoutIn: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.bottom_layout_in).apply {
+            fillAfter = true
+        }
+    }
+
+    private val bottomLayoutOut: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.bottom_layout_out).apply {
+            fillAfter = true
+        }
+    }
+
+    private val alpha01: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.alpha_0_1).apply {
+            fillAfter = true
+        }
+    }
+
+    private val alpha10: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.alpha_1_0).apply {
+            fillAfter = true
+        }
+    }
+
     override fun getLayoutRes() = R.layout.activity_play
 
     override fun initOnCreate(savedInstanceState: Bundle?) {
         // 空出StatusBar的高度
         playTopLayout.layoutParams = playTopLayout.layoutParams.apply {
-            (this as ConstraintLayout.LayoutParams).topMargin = statusBarHeight
+            (this as LinearLayout.LayoutParams).topMargin = statusBarHeight
         }
 
         backBtn.clicks().`as`(bindLifecycle()).subscribe {
             onBackPressed()
         }
+
+        // 模糊背景图片
+        Observable
+                .create<Bitmap> {
+                    try {
+                        it.onNext(BitmapFactory
+                                .decodeResource(resources, R.drawable.test_play_img)
+                                .blur(this))
+                        it.onComplete()
+                    } catch (e: Exception) {
+                        it.onError(e)
+                    }
+                }
+                .subscribeOn(Schedulers.io())
+                .toMainThread()
+                .`as`(bindLifecycle())
+                .subscribe({
+                    playRootView.backgroundDrawable = BitmapDrawable(resources, it)
+                }) {
+                    it.printStackTrace()
+                }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // 执行一下动画
+        playTopLayout.startAnimation(topLayoutIn)
+        playBottomLayout.startAnimation(bottomLayoutIn)
+        minLrcLayout.startAnimation(alpha01)
+    }
+
+    override fun onBackPressed() {
+        // 执行动画  需要反着执行
+        playTopLayout.startAnimation(topLayoutOut)
+        playBottomLayout.startAnimation(bottomLayoutOut)
+        minLrcLayout.startAnimation(alpha10)
+        super.onBackPressed()
     }
 
 }
